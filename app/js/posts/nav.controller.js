db6cb5fb874c4708a7d91ff5c8656bd04503462e
Vi Pham <tvpham11@gmail.com>
2015-07-05 13:45:02 -0400

    scaffolded nav controller js file

---
+++ app/js/posts/nav.controller.js
@@ -0,0 +1,15 @@
+(function (){
+
+  'use strict';
+
+  angular.module('CodeBytes')
+
+  .controller('NavCtrl', ['$scope', 'PostService',
+
+    function ($scope, PostService) {
+
+    }
+
+  ])
+
+}());
