44a7528aaa6475944b8a47d28590d95b0c332238
Meade Slocomb <meade.slocomb@gmail.com>
2015-07-04 21:35:27 -0400

    Added beginnings of file structure

--- app/js/main.js
+++ app/js/main.js
@@ -1,1 +1,35 @@
-console.log('Angular Rocks', angular);
+(function() {
+  'use strict';
+
+  angular.module('CodeBytes', ['ui.router'])
+
+  .config([ '$stateProvider', '$urlRouterProvider',
+    function ($stateProvider, $urlRouterProvider) {
+
+      $urlRouterProvider.otherwise('/');
+
+      $stateProvider
+        .state('signup', {
+          url: '/signup',
+          templateUrl: 'js/templates/signup.html',
+          controller: 'SignUpController'
+        })
+        .state('signin', {
+          url: '/signin',
+          templateUrl: 'js/templates/signin.html',
+          controller: 'SignInController'
+        })
+        .state('feed', {
+          url: '/feed',
+          templateUrl: 'js/templates/feed.html',
+          controller: 'FeedController'
+        .state('profile', {
+          url: '/profile :id',
+          templateUrl: 'js/templates/profile.html',
+          controller: 'ProfileController'
+        })
+        });
+    }
+  ]);
+
+}());
