e3202eb6f8b28cc76f2714150e7dbfd9330a6592
Vi Pham <tvpham11@gmail.com>
2015-07-07 11:24:14 -0400

    got github authorization request window to pop up upon clicking on sign-in with github

--- app/js/users/user.controller.js
+++ app/js/users/user.controller.js
@@ -3,9 +3,9 @@
 
   angular.module('CodeBytes')
 
-  .controller('UserController', ['$scope', 'UserService',
+  .controller('UserController', ['$scope', 'UserService', '$auth',
 
-    function($scope, UserService) {
+    function($scope, UserService, $auth) {
 
       $scope.registerUser = function() {
 
@@ -14,6 +14,21 @@
       $scope.loginUser = function() {
 
       };
+
+      $scope.authenticate = function(provider) {
+        $auth.authenticate(provider);
+      };
+
+      // Check if logged in
+      $scope.isAuthenticated = function() {
+
+      };
+
+      // Connect account with GitHub
+      $scope.linkGithub = function() {
+
+      };
+
 
     }
 
