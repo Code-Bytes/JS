a16f933500344d4b4f5ddd5f92ad08bbe08ba02e
Vi Pham <tvpham11@gmail.com>
2015-07-12 00:47:45 -0400

    moved logic from nav controller to user service though current user avatar is not displaying, resulting in drop-down menu not showing up either

---
+++ app/js/users/nav.controller.js
@@ -0,0 +1,35 @@
+(function (){
+
+  'use strict';
+
+  angular.module('CodeBytes')
+
+  .controller('NavController', ['UserService','$scope', '$auth',
+
+    function (UserService, $scope, $auth) {
+
+      $scope.githubLogin = function() {
+        UserService.ghLogin();
+      };
+
+      $scope.isAuthenticated = function() {
+        return $auth.isAuthenticated();
+      };
+
+      $scope.avatar = function(){
+        UserService.getAvatar();
+      };
+
+      $scope.avatarLink = function(){
+        UserService.avatarUrl();
+      };
+
+      $scope.logout = function() {
+        UserService.logOut();
+      };
+
+    }
+
+  ]);
+
+}());
--- app/js/users/user.service.js
+++ app/js/users/user.service.js
@@ -3,11 +3,54 @@
 
   angular.module('CodeBytes')
 
-  .service('UserService', ['$http', '$location',
+  .service('UserService', ['$http', '$location', '$auth', '$window', '$rootScope', '$sce',
 
-    function ($http, $location) {
+    function ($http, $location, $auth, $window, $rootScope, $sce) {
 
+      var isLoggedIn = function() {
+        if ($auth.getToken() !== null){
+          return true;
+        } else {
+          return false;
+        }
+      };
 
+      this.ghLogin = function() {
+        $auth.authenticate('github')
+          .then(function(response) {
+            $window.localStorage.currentUser = JSON.stringify(response.data.user);
+            $rootScope.currentUser = JSON.parse($window.localStorage.currentUser);
+            $rootScope.token = $auth.getToken();
+            $location.path('/');
+          })
+          .catch(function(response) {
+            // console.log(response.data);
+          });
+      };
+
+      this.getAvatar = function() {
+        var currentUser = JSON.parse(localStorage.getItem("currentUser"));
+        if (isLoggedIn() === true) {
+          return $sce.trustAsHtml('<img class="avatar" src="' + currentUser.avatar + '">');
+        } else {
+          return $sce.trustAsHtml('<i class="fa fa-user fa-2x portrait"></i>');
+        }
+      };
+
+      this.avatarUrl = function() {
+        if (isLoggedIn() === true){
+          return '/#/myprofile';
+        } else {
+          return '/#/login';
+        }
+      };
+
+      this.logOut = function() {
+        $auth.logout();
+        delete $window.localStorage.currentUser;
+        $rootScope.token = null;
+        $location.path('/');
+      };
 
     }
 
