5daa0fd2246c6dd3c631614ed2a5ea995806c775
Meade Slocomb <meade.slocomb@gmail.com>
2015-07-18 18:12:27 -0400

    Displayed number of comments on each post in the main feed

--- app/js/posts/feed.controller.js
+++ app/js/posts/feed.controller.js
@@ -3,9 +3,11 @@
 
   angular.module('CodeBytes')
 
-  .controller('FeedController', ['PostService', '$scope', '$rootScope',
+  .controller('FeedController', ['PostService', '$scope', '$rootScope', '$http', '$auth',
 
-    function (PostService, $scope, $rootScope) {
+    function (PostService, $scope, $rootScope, $http, $auth) {
+
+      $scope.token = $auth.getToken();
 
       $scope.isLoggedIn = function () {
         var currentUser = JSON.parse(localStorage.getItem("currentUser"));
@@ -18,6 +20,25 @@
 
       $rootScope.$on('PostsReceived', function (event, data) {
         $scope.feed = data;
+
+        _.each($scope.feed, function(postInFeed){
+          postInFeed.numberOfComments = 0;
+          $http({
+          url: 'https://pacific-hamlet-4796.herokuapp.com/posts/' + postInFeed.id + '/comments',
+          headers: {
+            'Authorization': $scope.token
+          },
+          method: 'GET'
+          })
+          .success(function(data){
+
+            _.each(data.comments, function(commentsOnPost){
+              postInFeed.numberOfComments ++;
+            });
+          });
+        });
+
+        console.log($scope.feed);
 
       $scope.upvote = PostService.upvote;
 
